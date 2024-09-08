35379ead5c2fd2483bc116e20784638ad682a8ea
Amit Pratap Singh <apjadon7895@gmail.com>
2024-09-08 10:50:38 +0530

    Initial commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "A class is a type of function, but instead of using the keyword function to initiate it, which keyword do we use?",
+        options:[ "Constructor", "Class","Object","DataObject"],
+        answer: 2
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "Which of the following acts as the input of a class-based component?",
+        options: ["Class", "Factory","Render","Props"],
+        answer:4
+    },
+    {
+        question: "Which of the following keyword is used to create a class inheritance?",
+        options: ["Create", "Inherits", "Extends", "This"],
+        answer:3
+    },
+];
